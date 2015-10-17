a7074159210c88ca0d11567d5c2996722c23b872
JanWilliem <smithw1989@icloud.com>
2015-10-17 16:36:28 +0800

    add comment

--- src/lintcode/StrStr.java
+++ src/lintcode/StrStr.java
@@ -1,6 +1,30 @@
 package lintcode;
 
 /**
+ * Easy strStr
+
+18% Accepted
+strstr (a.k.a find sub string), is a useful function in string operation. Your task is to implement this function.
+
+For a given source string and a target string, you should output the first index(from 0) of target string in source string.
+
+If target does not exist in source, just return -1.
+
+Have you met this question in a real interview? Yes
+Example
+If source = "source" and target = "target", return -1.
+
+If source = "abcdabcdefg" and target = "bcd", return 1.
+
+Challenge
+O(n2) is acceptable. Can you implement an O(n) algorithm? (hint: KMP)
+
+Clarification
+Do I need to implement KMP Algorithm in a real interview?
+
+Not necessary. When you meet this problem in a real interview, the interviewer may just want to test your basic implementation ability. But make sure your confirm with the interviewer first.
+ */
+/**
  * 容易 字符串查找
  * 
  * 18% 通过 字符串查找（又称查找子字符串），是字符串操作中一个很有用的函数。你的任务是实现这个函数。
